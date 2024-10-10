6665a3f741d90d8346f8f178cd49e2394814c022
KazuchikaNakagawa <KazuchikaNakagawa@gmail.com>
2024-10-10 10:41:57 +0900

    reimplemented sourcelocation

--- support/sourcelocation.cpp
+++ support/sourcelocation.cpp
@@ -5,7 +5,7 @@ namespace flyplum {
 
 std::u8string SourceLocation::toString() const
 {
-    return u8"Path: " + *path + u8"(" + to_u8string(line) + u8"," + to_u8string(column) + u8")";
+    return *path + u8":" + flyplum::to_u8string(line) + u8":" + flyplum::to_u8string(column);
 }
 
 }
